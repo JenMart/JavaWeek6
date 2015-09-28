2c47df1895a6e6230e8a7bee12f47079ff7f3659
jensinamart <jensinamart@gmail.com>
2015-09-28 13:10:00 -0500

    This.....!

---
+++ Dinosaur.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jensinamart on 9/28/15.
+ */
+public class Dinosaur {
+
+    public void Roar(){
+        System.out.println("Roar!");
+    }
+}
---
+++ DinosaurSupervisor.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jensinamart on 9/28/15.
+ */
+public class DinosaurSupervisor {
+    public static void main(String[] args) {
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velocirator = new Dinosaur();
+
+        t_rex.Roar();
+        velocirator.Roar();
+    }
+
+}
